f287ba7e9f0acab0809758df349e77c0917572e8
Jesus Gomez <jesusgp22@gmail.com>
2017-10-02 17:58:47 -0400

    Add navbar and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import './../styles/navbar.css'
+
 export const Header = () => (
-    <header>
-        <nav>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/portfolio'>Portfolio</Link></li>
-                <li><Link to='/blog'>blog</Link></li>
-            </ul>
+    <header className="container">
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav navbar-center">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#"><i className="fa fa-lightbulb-o"></i> Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">About me</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Portfolio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Blog</a>
+                    </li>
+                </ul>
+                <ul className="navbar-nav lr-auto">
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i className="fa fa-language"></i>
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a className="dropdown-item" href="#">English</a>
+                            <a className="dropdown-item" href="#">Spanish</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </header>
 )
