5c0b51db48d10f22991620f318026395febcf916
Jesus Gomez <jesusgp22@gmail.com>
2017-10-02 14:47:03 -0400

    Initial commit for personal site

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => (
+    <div>
+        <h1>Jesus Gomez</h1>
+        <h4>Software Engineer, Full Stack Developer</h4>
+    </div>
+)
+
+export default Home
