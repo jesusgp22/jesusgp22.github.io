dfd8fd209c8ec94e474ef8284730d936d2d3394f
Jesus Gomez <jesusgp22@gmail.com>
2017-10-03 17:31:00 -0400

    Update header with links

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import './../styles/navbar.css'
 
 export const Header = () => (
-    <header className="container">
+    <header className="container-fluid">
         <nav className="navbar navbar-expand-lg navbar-light">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -13,26 +13,26 @@ export const Header = () => (
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav navbar-center">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#"><i className="fa fa-lightbulb-o"></i> Home <span className="sr-only">(current)</span></a>
+                        <Link className="nav-link" to="/"><i className="fa fa-lightbulb-o"></i> Home <span className="sr-only">(current)</span></Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">About me</a>
+                        <Link className="nav-link" to="/about">About me</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Portfolio</a>
+                        <Link className="nav-link" to="/portfolio">Portfolio</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Blog</a>
+                        <Link className="nav-link" to="/blog">Blog</Link>
                     </li>
                 </ul>
                 <ul className="navbar-nav lr-auto">
                     <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <button className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i className="fa fa-language"></i>
-                        </a>
+                        </button>
                         <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a className="dropdown-item" href="#">English</a>
-                            <a className="dropdown-item" href="#">Spanish</a>
+                            <button className="dropdown-item">English</button>
+                            <button className="dropdown-item">Spanish</button>
                         </div>
                     </li>
                 </ul>
